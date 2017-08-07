bebdde347fcdf761d8aca8344495ecc6d6ccd37b
Lebeza Wendemsesha <BezMack@Lebezas-MBP.fios-router.home>
2017-08-07 19:33:46 -0400

    add files

---
+++ keys.js
@@ -0,0 +1,8 @@
+console.log("Keys loaded");
+
+exports.twitterKeys = {
+  consumer_key: '<4tfxAI2SBa7BFtFV66dB8aEhQ>',
+  consumer_secret: '<rvIc5hdr5l9ROQ1MhpvrikjWX3NATfPMQlG6FY0k7Of3eMVQxs>',
+  access_token_key: '<337337924-svc2ZuWBQ5hZnjCc5Gbf0K5uV6yVLGgYcFNxhUfH>',
+  access_token_secret: '<V0uTNR4KN5T2OI9T5LR0Q9eq6qPtcGogNz0M7STQTygAI>',
+}
